459ff2f087f7dbb1ea5af3d9296c5175c30dd541
Fernando Wals Rodriguez <ferwalrod@alum.us.es>
2021-03-01 14:26:04 +0100

    Implementado el tipo Dice y sus subtipos

--- Source/MiniRogue_TFG/Dice.cpp
+++ Source/MiniRogue_TFG/Dice.cpp
@@ -2,13 +2,19 @@
 
 
 #include "Dice.h"
+#include "Engine/World.h"
+#include "Engine/Engine.h"
+#include "MiniRogue_TFGGameModeBase.h"
 
 // Sets default values
 ADice::ADice()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	Dice = CreateDefaultSubobject<UStaticMeshComponent>("Dice");
+	SetRootComponent(Dice);
+	Dice->OnComponentSleep.AddDynamic(this,&ADice::OnSleep);
+	Dice->SetSimulatePhysics(true);
 }
 
 // Called when the game starts or when spawned
@@ -23,5 +29,22 @@ void ADice::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void ADice::OnSleep(UPrimitiveComponent* sleeper, FName Bone)
+{
+	Hightest = -9999.f;
+	TArray<FName> sockets = Dice->GetAllSocketNames();
+	for (FName socket : sockets) {
+		float Z= Dice->GetSocketLocation(socket).Z;
+		if (Z > Hightest) {
+			Hightest = Z;
+			FaceShowing = socket;
+		}
+	}
+	AMiniRogue_TFGGameModeBase* GM = Cast<AMiniRogue_TFGGameModeBase>(GetWorld()->GetAuthGameMode());
+	if (GM) {
+		GM->Results.Add(Type, ValueMapping[FaceShowing]);
+	}
 }
 
